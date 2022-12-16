cb256a49af5b88041aedef75b824568e4f944176
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2022-12-16 20:27:19 +0800

    update home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Navbar from "./Components/Navbar";
-import Routeses from "./Routes/index";
+import Routes from "./Routes/index";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 
-			<div className="container">
-				<Routeses />
+			<div className="main-container">
+				<Routes />
 
 				<div class="line top"></div>
 				<div class="line bottom"></div>
