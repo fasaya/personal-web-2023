08e7910477f9a73977cedea86b688b32e27be277
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2022-12-17 19:25:26 +0800

    update blog

---
+++ src/Pages/Errors/UnderDevelopment.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { UnderConstruction } from "../../Components/Errors/UnderConstruction";
+
+const UnderDevelopment = () => (
+	<section className="bg-white dark:bg-custom-grey h-full">
+		<div className="grid h-screen place-items-center text-custom-grey-light">
+			{/* <UnderConstruction /> */}
+			<h1>Under Development</h1>
+		</div>
+	</section>
+);
+
+export default UnderDevelopment;
--- src/Routes/HomeRoute.js
+++ src/Routes/HomeRoute.js
@@ -3,13 +3,15 @@ import Home from "../Pages/Home";
 import Blog from "../Pages/Blog";
 import Portfolio from "../Pages/Portfolio";
 import NotFound from "../Pages/Errors/NotFound";
+import UnderDevelopment from "../Pages/Errors/UnderDevelopment";
 
 export default function HomeRoute() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
-			<Route path="/blog" element={<Blog />} />
 			<Route path="/portfolio" element={<Portfolio />} />
+			{/* <Route path="/blog" element={<Blog />} /> */}
+			<Route path="/blog" element={<UnderDevelopment />} />
 
 			<Route path="*" element={<NotFound />} />
 		</Routes>
