347a2c26b33dfaaa26a40cd05a5c086bb21ffc50
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-01-09 11:21:09 +0800

    lexical not working
    https://codesandbox.io/s/lexical-rich-text-example-5tncvy?file=/src/styles.css

--- src/Pages/User/Blog/Create.js
+++ src/Pages/User/Blog/Create.js
@@ -1,60 +1,8 @@
-import { $getRoot, $getSelection } from "lexical";
 import React, { useEffect, useState } from "react";
+import Editor from "../../../Components/Editor";
 // import Side from "./Side";
 
-// import { LexicalComposer } from "@lexical/react/LexicalComposer";
-// import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
-// import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-// import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-// import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-// import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-// import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-
-const theme = {
-	// Theme styling goes here
-};
-
-// // When the editor changes, you can get notified via the
-// // LexicalOnChangePlugin!
-// function onChange(editorState) {
-// 	editorState.read(() => {
-// 		// Read the contents of the EditorState here.
-// 		const root = $getRoot();
-// 		const selection = $getSelection();
-
-// 		console.log(root, selection);
-// 	});
-// }
-
-// // Lexical React plugins are React components, which makes them
-// // highly composable. Furthermore, you can lazy load plugins if
-// // desired, so you don't pay the cost for plugins until you
-// // actually use them.
-// function MyCustomAutoFocusPlugin() {
-// 	const [editor] = useLexicalComposerContext();
-
-// 	useEffect(() => {
-// 		// Focus the editor when the effect fires!
-// 		editor.focus();
-// 	}, [editor]);
-
-// 	return null;
-// }
-
-// // Catch any errors that occur during Lexical updates and log them
-// // or throw them as needed. If you don't throw them, Lexical will
-// // try to recover gracefully without losing user data.
-// function onError(error) {
-// 	console.error(error);
-// }
-
 const BlogCreate = () => {
-	// const initialConfig = {
-	// 	namespace: "MyEditor",
-	// 	theme,
-	// 	onError,
-	// };
-
 	useEffect(() => {
 		//
 	}, []);
@@ -71,19 +19,8 @@ const BlogCreate = () => {
 					</div>
 				</div>
 
-				<div className="col-span-1">{/* <Side /> */}</div>
-
-				<div className="col-span-3">
-					{/* <LexicalComposer initialConfig={initialConfig}>
-						<PlainTextPlugin
-							contentEditable={<ContentEditable />}
-							placeholder={<div>Enter some text...</div>}
-							ErrorBoundary={LexicalErrorBoundary}
-						/>
-						<OnChangePlugin onChange={onChange} />
-						<HistoryPlugin />
-						<MyCustomAutoFocusPlugin />
-					</LexicalComposer> */}
+				<div className="col-span-4 bg-white">
+					<Editor />
 				</div>
 			</div>
 		</section>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 import "./Assets/Styles/index.css";
+import "./Components/Editor/styles.css";
 import Main from "./Layouts/Main";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
