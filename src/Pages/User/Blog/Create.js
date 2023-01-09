e69c79d1287cf6461b2194a55fe1962c4cd593d7
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-01-09 12:16:21 +0800

    use draft js

--- src/Pages/User/Blog/Create.js
+++ src/Pages/User/Blog/Create.js
@@ -1,89 +1,101 @@
-import { $getRoot, $getSelection } from "lexical";
 import React, { useEffect, useState } from "react";
-// import Side from "./Side";
+import { EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
 
-// import { LexicalComposer } from "@lexical/react/LexicalComposer";
-// import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
-// import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-// import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-// import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-// import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-// import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-
-const theme = {
-	// Theme styling goes here
-};
-
-// // When the editor changes, you can get notified via the
-// // LexicalOnChangePlugin!
-// function onChange(editorState) {
-// 	editorState.read(() => {
-// 		// Read the contents of the EditorState here.
-// 		const root = $getRoot();
-// 		const selection = $getSelection();
-
-// 		console.log(root, selection);
-// 	});
-// }
-
-// // Lexical React plugins are React components, which makes them
-// // highly composable. Furthermore, you can lazy load plugins if
-// // desired, so you don't pay the cost for plugins until you
-// // actually use them.
-// function MyCustomAutoFocusPlugin() {
-// 	const [editor] = useLexicalComposerContext();
-
-// 	useEffect(() => {
-// 		// Focus the editor when the effect fires!
-// 		editor.focus();
-// 	}, [editor]);
-
-// 	return null;
-// }
-
-// // Catch any errors that occur during Lexical updates and log them
-// // or throw them as needed. If you don't throw them, Lexical will
-// // try to recover gracefully without losing user data.
-// function onError(error) {
-// 	console.error(error);
-// }
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const BlogCreate = () => {
-	// const initialConfig = {
-	// 	namespace: "MyEditor",
-	// 	theme,
-	// 	onError,
-	// };
+	const [editorState, setEditorState] = React.useState(() =>
+		EditorState.createEmpty()
+	);
+
+	const [convertedContent, setConvertedContent] = useState(null);
 
 	useEffect(() => {
-		//
-	}, []);
+		let html = convertToHTML(editorState.getCurrentContent());
+		setConvertedContent(html);
+		console.log(html);
+	}, [editorState]);
 
 	return (
 		<section className="bg-custom-grey h-full mt-24 mb-20 md:mt-40 md:mb-32 mx-20">
-			<div className="mx-auto grid grid-cols-4 gap-4 w-4/5">
-				<div className="col-span-4 mb-2">
+			<div className="mx-auto grid gap-4 w-4/5">
+				<div className="mb-2">
 					<div className="content text-center sm:text-left">
 						<div className="title">
 							<div className="title_inner !text-xl">Blog</div>
-							<p className="text-md text-gray-400">Create</p>
+							<p className="text-md text-gray-400">
+								Create Something
+							</p>
 						</div>
 					</div>
 				</div>
 
-				<div className="col-span-1">{/* <Side /> */}</div>
+				<div className="w-full md:w-1/3  mb-6 md:mb-0">
+					<label
+						className="block uppercase tracking-wide text-custom-grey-light text-xs font-bold mb-2"
+						htmlFor="title"
+					>
+						Title
+					</label>
+					<input
+						className="appearance-none block w-full bg-gray-200 text-custom-grey-light border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+						type="text"
+						placeholder="Title"
+					/>
+				</div>
 
-				<div className="col-span-3">
-					{/* <LexicalComposer initialConfig={initialConfig}>
-						<PlainTextPlugin
-							contentEditable={<ContentEditable />}
-							placeholder={<div>Enter some text...</div>}
-							ErrorBoundary={LexicalErrorBoundary}
+				<div className="">
+					<label className="text-custom-grey-light">Content:</label>
+					<div className="bg-white rounded">
+						<Editor
+							editorState={editorState}
+							onEditorStateChange={setEditorState}
+							wrapperClassName="wrapper-class"
+							editorClassName="editor-class"
+							toolbarClassName="toolbar-class"
 						/>
-						<OnChangePlugin onChange={onChange} />
-						<HistoryPlugin />
-						<MyCustomAutoFocusPlugin />
-					</LexicalComposer> */}
+					</div>
+				</div>
+
+				<div className="w-full md:w-1/3 mb-6 md:mb-0">
+					<label
+						className="block uppercase tracking-wide text-custom-grey-light text-xs font-bold mb-2"
+						htmlFor="grid-state"
+					>
+						State
+					</label>
+					<div className="relative">
+						<select
+							className="block appearance-none w-full bg-gray-200 border border-gray-200 text-custom-grey-light py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+							id="grid-state"
+						>
+							<option>New Mexico</option>
+							<option>Missouri</option>
+							<option>Texas</option>
+						</select>
+						<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-custom-grey-light">
+							<svg
+								className="fill-current h-4 w-4"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+							>
+								<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+							</svg>
+						</div>
+					</div>
+				</div>
+
+				<div className="w-28">
+					<button
+						type="button"
+						className="btn fill mb-3"
+						style={{ width: "100%" }}
+					>
+						Save
+					</button>
 				</div>
 			</div>
 		</section>
